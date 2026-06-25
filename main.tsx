d0f5bc5e203501b38ebeee81af85fbb4045b0741
Luiz Carlos Rezende da Silva <214377491+luizcrezende@users.noreply.github.com>
2026-06-25 05:36:50 -0300

    Create main.tsx
    Signed-off-by: Luiz Carlos Rezende da Silva <214377491+luizcrezende@users.noreply.github.com>

---
+++ main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// Importa o seu componente (Ajuste o nome do arquivo abaixo se necessário)
+import App from './terminal_industrial_guarda_soberana.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
