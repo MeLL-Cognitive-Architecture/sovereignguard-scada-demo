a74140dce86afc80c444c38afb4bd9745357c066
Luiz Carlos Rezende da Silva <214377491+luizcrezende@users.noreply.github.com>
2026-06-25 05:43:27 -0300

    Update main.tsx
    Signed-off-by: Luiz Carlos Rezende da Silva <214377491+luizcrezende@users.noreply.github.com>

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Importa o seu componente (Ajuste o nome do arquivo abaixo se necessário)
-import App from './terminal_industrial_guarda_soberana.tsx'
+// Mude a linha abaixo para o nome EXATO do seu arquivo, MAS SEM o ".tsx" no final
+import App from './terminal_industrial_guarda_soberana'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
